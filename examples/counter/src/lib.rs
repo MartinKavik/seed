597b8b877cbf4af77b3e4e11ba415a98781f6e9f
Martin Kavík <martin@kavik.cz>
2020-06-19 11:28:33 +0200

    demo: fetch all users

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -1,38 +1,58 @@
-//! A simple, cliché example demonstrating structure and syntax.
-//! Inspired by [Elm example](https://guide.elm-lang.org/architecture/buttons.html).
-
-// Some Clippy linter rules are ignored for the sake of simplicity.
-#![allow(clippy::needless_pass_by_value, clippy::trivially_copy_pass_by_ref)]
-
 use seed::{prelude::*, *};
+use serde::Deserialize;
 
 // ------ ------
 //     Init
 // ------ ------
 
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    let user_session = UserSession {
+        token: "xxx".to_owned(),
+    };
+    orders.perform_cmd(async { Msg::FetchAllUsers(fetch_all_users(user_session).await) });
+    Model
+}
+
+async fn fetch_all_users(session: UserSession) -> fetch::Result<Vec<Entry<User>>> {
+    Request::new("http://localhost:8000/api/users")
+        .header(Header::authorization(session.token))
+        .fetch()
+        .await?
+        .check_status()?
+        .json()
+        .await
 }
 
 // ------ ------
 //     Model
 // ------ ------
 
-type Model = i32;
+struct Model;
+
+struct UserSession {
+    token: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Entry<T> {
+    value: T,
+}
+
+#[derive(Debug, Deserialize)]
+struct User;
 
 // ------ ------
 //    Update
 // ------ ------
 
 enum Msg {
-    Increment,
-    Decrement,
+    FetchAllUsers(fetch::Result<Vec<Entry<User>>>),
 }
 
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
-        Msg::Decrement => *model -= 1,
+        Msg::FetchAllUsers(Ok(users)) => log!(users),
+        Msg::FetchAllUsers(Err(fetch_error)) => log!(fetch_error),
     }
 }
 
@@ -40,12 +60,8 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 //     View
 // ------ ------
 
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        button![ev(Ev::Click, |_| Msg::Decrement), "-"],
-        div![model],
-        button![ev(Ev::Click, |_| Msg::Increment), "+"],
-    ]
+fn view(_: &Model) -> Node<Msg> {
+    div!["Placeholder"]
 }
 
 // ------ ------
