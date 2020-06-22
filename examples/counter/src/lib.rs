fa6dfee8bd760bed952d42c8b89820edf7374167
Martin Kavík <martin@kavik.cz>
2020-06-22 09:11:13 +0200

    demo: Input number

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -1,56 +1,53 @@
-//! A simple, cliché example demonstrating structure and syntax.
-//! Inspired by [Elm example](https://guide.elm-lang.org/architecture/buttons.html).
-
-// Some Clippy linter rules are ignored for the sake of simplicity.
-#![allow(clippy::needless_pass_by_value, clippy::trivially_copy_pass_by_ref)]
-
 use seed::{prelude::*, *};
 
-// ------ ------
-//     Init
-// ------ ------
-
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+    Model {
+        number: 0,
+        input_element: ElRef::new(),
+    }
 }
 
-// ------ ------
-//     Model
-// ------ ------
-
-type Model = i32;
-
-// ------ ------
-//    Update
-// ------ ------
+struct Model {
+    number: i32,
+    input_element: ElRef<web_sys::HtmlInputElement>,
+}
 
 enum Msg {
-    Increment,
-    Decrement,
+    InputChanged(i32),
+    ResetInputElement,
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
-        Msg::Decrement => *model -= 1,
+        Msg::InputChanged(number) => {
+            log!(number);
+            model.number = number;
+        }
+        Msg::ResetInputElement => {
+            let element = model.input_element.get().expect("input element");
+            element.set_value("");
+        }
     }
 }
 
-// ------ ------
-//     View
-// ------ ------
-
 fn view(model: &Model) -> Node<Msg> {
     div![
-        button![ev(Ev::Click, |_| Msg::Decrement), "-"],
-        div![model],
-        button![ev(Ev::Click, |_| Msg::Increment), "+"],
+        input![
+            el_ref(&model.input_element),
+            attrs!{
+                At::Type => "number",
+            },
+            ev(Ev::Input, |event| {
+                let element = event.target().unwrap().unchecked_into::<web_sys::HtmlInputElement>();
+                IF!(element.report_validity() => Msg::InputChanged(element.value().parse().unwrap_or_default()))
+            }),
+        ],
+        button![
+            "Reset",
+            ev(Ev::Click, |_| Msg::ResetInputElement),
+        ]
     ]
 }
-
-// ------ ------
-//     Start
-// ------ ------
 
 #[wasm_bindgen(start)]
 pub fn start() {
